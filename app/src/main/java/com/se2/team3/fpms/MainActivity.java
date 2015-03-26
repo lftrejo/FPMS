f89d859936387b5b7ef5190c698bed79a34d923a
Carlos Corral <cacorral@miners.utep.edu>
2015-03-26 10:04:36 -0600

    Upload aircraft, add, view details, manage flight plans, and manage resources

--- app/src/main/java/com/se2/team3/fpms/MainActivity.java
+++ app/src/main/java/com/se2/team3/fpms/MainActivity.java
@@ -1,34 +1,142 @@
 package com.se2.team3.fpms;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    EditText txtName, txtAircraftID, txtAircraftModel,txtAircraftColor,
+            txtMinimumSpeed, txtNormalSpeed, txtMaximumSpeed,
+            txtMinimumAltitude, txtNormalAltitude, txtMaximumAltitude,
+            txtFuelConsumption, txtFuelConsumptionRate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.add_aircraft);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        txtName = (EditText) findViewById(R.id.textName);
+        txtAircraftID = (EditText) findViewById(R.id.textAircraftID);
+        txtAircraftModel = (EditText) findViewById(R.id.textAircraftModel);
+        txtAircraftColor = (EditText) findViewById(R.id.textAircraftColor);
+        txtMinimumSpeed = (EditText) findViewById(R.id.textMinimumSpeed);
+        txtNormalSpeed = (EditText) findViewById(R.id.textNormalSpeed);
+        txtMaximumSpeed = (EditText) findViewById(R.id.textMaximumSpeed);
+        txtMinimumAltitude = (EditText) findViewById(R.id.textMinimumAltitude);
+        txtNormalAltitude = (EditText) findViewById(R.id.textNormalAltitude);
+        txtMaximumAltitude = (EditText) findViewById(R.id.textMaximumAltitude);
+        txtFuelConsumption = (EditText) findViewById(R.id.textFuelConsumption);
+        txtFuelConsumptionRate = (EditText) findViewById(R.id.textFuelConsumptionRate);
+
+        final Button btnHome = (Button) findViewById(R.id.buttonHome);
+        final Button btnSave = (Button) findViewById(R.id.buttonSave);
+        Button btnBack = (Button) findViewById(R.id.buttonBack);
+
+        txtName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+
+        txtAircraftID.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+
+        txtAircraftModel.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+        txtAircraftColor.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+        txtMinimumSpeed.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+        txtNormalSpeed.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+        txtMaximumSpeed.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+        txtMinimumAltitude.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+        txtNormalAltitude.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+        txtMaximumAltitude.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+        txtFuelConsumption.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
+
+        txtFuelConsumptionRate.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnSave.setEnabled(!s.equals(""));
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
