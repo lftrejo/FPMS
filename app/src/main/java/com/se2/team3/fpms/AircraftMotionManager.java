56114116a746d2d578a0da1b0b60be38e2b3b18c
Mater Scott <Me25@Master.rul>
2015-03-30 20:22:28 -0600

    d

--- app/src/main/java/com/se2/team3/fpms/AircraftMotionManager.java
+++ app/src/main/java/com/se2/team3/fpms/AircraftMotionManager.java
@@ -10,22 +10,42 @@ public class AircraftMotionManager
     private static AircraftMotionManager instance;
     private ArrayList<AircraftMotionListener> listeners = new ArrayList<AircraftMotionListener>();
 
+    private AircraftMotionManager() {
+        new Thread(new Runnable() {
+            public void run() {
+                // Initialize
+                initLocationProvider();
+                initWindProvider();
+            }
+        }).start();
+
+    // get Singleton
     public static synchronized AircraftMotionManager getInstance() {
-        if (instance == null)
+        if (instance == null) {
             instance = new AircraftMotionManager();
 
+        }
+
         return instance;
+    }
+
+    // Initialize location provider when this created
+    // run in this thread not UI thread
+    void initLocationProvider()
+    {
+
+    }
+
+    // Initialize location provider when this created
+    // run in this thread not UI thread
+    void initWindProvider()
+    {
+
     }
 
 
 
 
-            new Thread(new Runnable() {
-                public void run() {
-                    Bitmap b = loadImageFromNetwork("http://example.com/image.png");
-                    mImageView.setImageBitmap(b);
-                }
-            }).start();
 
     }
 }
---
+++ app/src/main/java/com/se2/team3/fpms/LocationListener.java
@@ -0,0 +1,9 @@
+package com.se2.team3.fpms;
+
+/**
+ * Created by Scott on 3/30/2015.
+ */
+public interface LocationListener {
+    // callback triggered when location provider signals location of aircraft change
+    void onLocationChanged();
+}
